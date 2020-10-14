fab47ea63681ed1e810167ef1fc0783b723c77e7
kai-blt <blaitong@gmail.com>
2020-10-14 07:59:13 -0700

    styled form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Form from './components/Form'
+import styled from 'styled-components'
 import './App.css';
+
+//Styled components
+const AppContainer = styled.div`
+  width: 80%;
+  margin: auto;
+`;
+
+
 
 function App() {
   return (
-    <div className="App">
+    <AppContainer>
       <Form />
-    </div>
+    </AppContainer>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,37 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import * as yup from 'yup'
 
 
+
+//Styled components
+const FormContainer = styled.div`
+    display: flex;
+    flex-flow: column wrap;
+    justify-content: center;
+    align-content: center;
+    height: 30vh;
+    padding: 2%;
+    font-weight: 500;
+
+    input {
+        margin: 2%;
+    }
+
+    label {
+        margin: 4%;
+    }
+
+    button {
+        padding: 2%;
+        font-size: 1.5rem;
+        margin: 4% 0;
+    }
+`;
+
+
+
+//Default blank form 
 const defaultFormData = {
     name: '',
     email: '',
@@ -12,7 +42,10 @@ const defaultFormData = {
 
 export default function Form(props) {
     //Hold form values in state
-    const [formData, setFormData] = useState(defaultFormData)
+    const [formData, setFormData] = useState(defaultFormData);
+
+    //Disable button until form is complete
+    const [disabled, setDisabled] = useState(true);
 
     const handleForm = (evt) => {
         //Get target name, it's value and type and checked from field user interacted with
@@ -23,20 +56,26 @@ export default function Form(props) {
     }
 
     return(
-        <form onSubmit={null}>
-            <label>Name               
-                <input type="text" name="name" value={formData.name} onChange={handleForm} />
-            </label>
-            <label>Email            
-                <input type="email" name="email" value={formData.email} onChange={handleForm} />
-            </label>
-            <label>Password                
-                <input type="text" name="password" value={formData.password} onChange={handleForm} />
-            </label>
-            <label>TOS                
-                <input type="checkbox" name="agree" checked={formData.agree} onChange={handleForm} />
-            </label>
-            <button>Submit</button>
-        </form>
+        <FormContainer>
+            <form onSubmit={null}>
+                <label>Name               
+                    <input type="text" name="name" value={formData.name} onChange={handleForm} />
+                </label>
+                <br/>
+                <label>Email            
+                    <input type="email" name="email" value={formData.email} onChange={handleForm} />
+                </label>
+                <br/>
+                <label>Password                
+                    <input type="text" name="password" value={formData.password} onChange={handleForm} />
+                </label>
+                <br/>
+                <label>Agree to TOS                
+                    <input type="checkbox" name="agree" checked={formData.agree} onChange={handleForm} />
+                </label>
+                <br/>
+                <button disabled={disabled}>Submit</button>
+            </form>
+        </FormContainer>
     )
 }
