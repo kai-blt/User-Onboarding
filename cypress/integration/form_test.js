f4007c92b224d227d2a42f222fa91badcd0b3391
kai-blt <blaitong@gmail.com>
2020-10-15 12:47:41 -0700

    refactored tests to first check if inputs exists, and then if the inputs can be used

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -11,92 +11,95 @@ describe('Form Testing', () => {
     const emailField = () => cy.get('input[name="email"]');
     const passwordField = () => cy.get('input[name="password"]');
     const roleDropdown = () =>  cy.get('select[name="role"]');
-    const radioButton = () => cy.get('input[name="bestlanguage"]');
+    const radioButton = () => cy.get('input[value="Python"]');
     const tosCheckbox = () =>  cy.get('input[name="agree"]');
     const nameError = () => cy.get('.nameError');
     const emailError = () => cy.get('.emailError');
     const submitButton = () => cy.get('button')
 
-    
 
-    describe('Name Field Testing', function() {
-        //Arrange
-        it('Tests the name and it\'s error message if not enough characters', function() {
-            //Act   
-           nameField()
-           .type('Tes').should('have.value', 'Tes')
-            
-            //Assert
-            nameError().should('contain', 'Please enter a name greater than 4 characters')
+    describe('Form Inputs exist', () => {
+        it('checks if the element exists', () => {
+            nameField().should('exist');
+            emailField().should('exist');
+            passwordField().should('exist');
+            roleDropdown().should('exist');
+            radioButton().should('exist');
+            tosCheckbox().should('exist')
+        })       
+    })
+
+
+    describe('Inputs Can Be Used', () => {
+        it('Types in the name field and verifies correct value', () => {               
+            nameField()
+                .type('Testing')
+                .should('have.value', 'Testing');          
+        })
+
+        it('Types in the email field and verifies correct value', () => {               
+            emailField()
+                .type('test@test.com')
+                .should('have.value', 'test@test.com');          
+        })
+
+        it('Types in the password field and verifies correct value', () => {               
+            passwordField()
+                .type('testpassword12345')
+                .should('have.value', 'testpassword12345');          
+        })
+
+        it('Tests if the role dropdown works', () => {
+            roleDropdown()
+                .select('Front End Engineer')
+                .should('have.value', 'Front End Engineer')
+        })
+
+        it('Tests if radio button works', () => {
+            radioButton()
+                .click()
+                .should('have.value', 'Python');    
+        })
+
+        it('Tests if checkbox works', () => {
+            tosCheckbox()
+                .click()
+                .should('be.enabled');    
         })
     });
     
+  
     
-    describe('Email Field Testing', function() {
-        //Arrange
-        it('Tests the email field to see if it works as expected', function() {
-            //Act   
-            cy.get('input[name="email"]').type('email@email').should('have.value', 'email@email')
-    
-            //Assert
-            cy.get('.emailError').should('contain', 'Please enter a valid email address')
-        })
-    });
-    
-    describe('Password Field Testing', function() {
-        //Arrange
-        it('Tests the password field to see if it works as expected', function() {
-            //Act   
-            cy.get('input[name="password"]').type('123456789').should('have.value', '123456789')
-    
-            //Assert
-            cy.get('.passwordError').should('contain', 'Please create a strong password that is greater than 10 characters')
-        })
-    });
+     
+
     
     
-    describe('Role Dropdown Testing', function() {
-        //Arrange
-        it('Tests the role dropdown works', function() {
-            //Act   
-            cy.get('select[name="role"]').select('Front End Engineer') 
+    // describe('Tests TOS checkbox', function() {
+    //     //Arrange
+    //     it('Checks to see if the checkbox can be used', function() {
+    //         //Act   
+    //         cy.get('input[name="agree"]').click().should('be.enabled')
+    //     })
+    // });
+
+    // describe('Error message tests', () => {
+    //     it('leaves fields blank to test if errors show', () => {
+    //         cy.get('.passwordError').should('contain', 'Please create a strong password that is greater than 10 characters')
+    //     })
+    // })
+
     
-            //Assert
-            cy.get('.passwordError').should('contain', 'Please create a strong password that is greater than 10 characters')
-        })
-    });
+    // describe('Corrects all fields, submits form and checks for cleared fields', function() {
+    //     //Arrange
+    //     it('Makes fields viable and submits form', function() {
+    //         //Act   
+    //         cy.get('input[name="name"]').type('t').should('have.value', 'Test')
+    //         cy.get('input[name="email"]').type('.com').should('have.value', 'email@email.com')
+    //         cy.get('input[name="password"]').type('0').should('have.value', '1234567890')
+    //         cy.get('button').click()
+    //         cy.get('input[name="name"]').should('have.value', '')
     
-    describe('Radio Button Testing', function() {
-        //Arrange
-        it('Tests the radio buttons work', function() {
-            //Act   
-            cy.get('input[name="bestlanguage"]').click({ multiple: true }).should('have.value', 'Python')
-    
-            //Assert
-            cy.get('.passwordError').should('contain', 'Please create a strong password that is greater than 10 characters')
-        })
-    });
-    
-    
-    describe('Tests TOS checkbox', function() {
-        //Arrange
-        it('Checks to see if the checkbox can be used', function() {
-            //Act   
-            cy.get('input[name="agree"]').click().should('be.enabled')
-        })
-    });
-    
-    describe('Corrects all fields, submits form and checks for cleared fields', function() {
-        //Arrange
-        it('Makes fields viable and submits form', function() {
-            //Act   
-            cy.get('input[name="name"]').type('t').should('have.value', 'Test')
-            cy.get('input[name="email"]').type('.com').should('have.value', 'email@email.com')
-            cy.get('input[name="password"]').type('0').should('have.value', '1234567890')
-            cy.get('button').click()
-            cy.get('input[name="name"]').should('have.value', '')
-    
-        })
-    });
+    //     })
+    // });
     
 })
